19f3bb6733816c7ee526a1b73d77b038705206c5
Ribby-0254 <14286113+Ribby-0254@users.noreply.github.com>
2021-06-12 09:38:35 -0400

    Adds basic boilerplate code for running discord bot

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('Hello World! :\')');
+import Discord from 'discord.js';
+
+const client = new Discord.Client();
+
+client.once('ready', () => console.log('Ready!'));
+
+client.login('ENTER_BOT_TOKEN_HERE');
