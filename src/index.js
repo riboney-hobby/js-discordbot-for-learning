f5e4f04342334d112b15f26c8052b1f69c8ab6e0
4H1R <=>
2021-06-12 21:19:28 +0430

    Added say command

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-import Discord from 'discord.js';
+import Discord from "discord.js";
 
 const client = new Discord.Client();
 
-client.once('ready', () => console.log('Ready!'));
+client.once("ready", () => console.log("Ready!"));
 
-client.login('ENTER_BOT_TOKEN_HERE');
+client.login("ENTER_BOT_TOKEN_HERE");
+
+client.on("message", (msg) => {
+  if (msg.author.bot) return;
+  if (msg.content.toLowerCase().startsWith("?say")) {
+    let args = msg.content.split(" ").slice(1).join(" ");
+    msg.channel.send(args);
+  }
+});
