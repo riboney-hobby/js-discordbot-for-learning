61e0cb3cc3f921b1ba6639fd2038ab47d7ae2f98
Sarukei <xSarukei@gmail.com>
2021-06-16 19:36:10 -0500

    Add BotClient.loadEvents()

--- src/BotClient.js
+++ src/BotClient.js
@@ -1,5 +1,5 @@
 const { Client, Collection } = require("discord.js");
-const fs = require("fs");
+const fs = require("fs/promises");
 const path = require("path");
 
 /**
@@ -19,30 +19,28 @@ class BotClient extends Client {
   }
 
   /**
-   * Loads the commands
-   * @param {*} commandsPath
+   * Loads the commands assuming that the commands folder contains folders only, with commands inside them.
+   * @param {*} commandsPath - Path to the commands folder
    */
   async loadCommands(commandsPath) {
-    fs.readdir(commandsPath, (err, commandFolders) => {
-      if (err) return console.error(err);
+    try {
+      const commandFolders = await fs.readdir(commandsPath);
 
-      commandFolders.forEach((folder) => {
-        fs.readdir(path.resolve(commandsPath, folder), (err, commandFiles) => {
-          if (err) return console.error(err);
-          const javaScriptFiles = commandFiles.filter((file) =>
-            file.endsWith(".js")
-          );
+      for (const folder of commandFolders) {
+        const commandFiles = (
+          await fs.readdir(path.resolve(commandsPath, folder))
+        ).filter((file) => file.endsWith(".js"));
 
-          javaScriptFiles.forEach((file) => {
-            const command = require(path.resolve(commandsPath, folder, file));
-
-            this.commands.set(command.name, command);
-          });
-        });
-      });
-    });
-
-    return;
+        for (const file of commandFiles) {
+          const command = require(path.resolve(commandsPath, folder, file));
+          this.commands.set(command.name, command);
+        }
+      }
+    } catch (err) {
+      console.error(
+        `An error occured while loading commands from commands folder: ${err}`
+      );
+    }
   }
 
   /**
@@ -50,7 +48,25 @@ class BotClient extends Client {
    * @param {*} eventsPath
    */
   async loadEvents(eventsPath) {
-    return;
+    try {
+      const eventFiles = await (
+        await fs.readdir(eventsPath)
+      ).filter((file) => file.endsWith(".js"));
+
+      for (const file of eventFiles) {
+        const event = require(path.resolve(eventsPath, file));
+
+        if (event.once) {
+          this.once(event.name, (...args) => event.execute(...args, this));
+        } else {
+          this.on(event.name, (...args) => event.execute(...args, this));
+        }
+      }
+    } catch (err) {
+      console.error(
+        `An error occured while loading events from events folder: ${err}`
+      );
+    }
   }
 
   /**
