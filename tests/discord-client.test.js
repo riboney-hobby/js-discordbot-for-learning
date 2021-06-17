ae28b7515c5e7e4be83977afa9e348108bc94043
riboney <14286113+Ribby-0254@users.noreply.github.com>
2021-06-17 15:01:56 -0400

    Modifies discord-client.test.js

--- tests/discord-client.test.js
+++ tests/discord-client.test.js
@@ -8,8 +8,23 @@ afterAll(() => {
 
 // src: https://jestjs.io/docs/asynchronous#promises
 // Required for handling async operations
+// resolve() jest function did not work for some reason
 describe('Discord bot successfully connects', () =>{
   test('Should return token upon connection', () => {
-    return expect(bot.login()).resolves.toBe(process.env.BOT_TOKEN);
+    return bot.login().then((token) => {
+      expect(token).toBe(process.env.BOT_TOKEN);
+    });
   });
 });
+
+// describe('Discord bot successfully connects', () =>{
+//   test('Should return token upon connection', () => {
+//     return expect(bot.login()).resolves.toBe(process.env.BOT_TOKEN);
+//   });
+// });
+
+// describe('Discord bot successfully connects', () =>{
+//   test('Should return token upon connection', () => {
+//     expect(bot.login()).toBe(process.env.BOT_TOKEN);
+//   });
+// });
