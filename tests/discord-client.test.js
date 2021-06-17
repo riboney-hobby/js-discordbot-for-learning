92a56cf6fa169d6ab39343f0cd956600050b5135
riboney <14286113+Ribby-0254@users.noreply.github.com>
2021-06-17 03:59:39 -0400

    Adds comments to explain the async and discord related issues in jest

--- tests/discord-client.test.js
+++ tests/discord-client.test.js
@@ -1,12 +1,15 @@
-const BOT_TOKEN = 'replace-with-bot-config';
+const BOT_TOKEN = 'ODUzMjY0ODI5MDE5OTc5ODI2.YMS2zA.5TTkS4AAjK9jt7QolMHS1BkQbTw';
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+// Jest doesn't exit properly without closing down Discord connection first
 afterAll(() => {
   client.destroy();
 });
 
 // src: https://jestjs.io/docs/asynchronous#promises
+// Required for handling async operations
+// resolve() jest function did not work for some reason
 describe('discord bot connection', () =>{
   test('Should return token upon connection', () => {
     return client.login(BOT_TOKEN).then((token) => {
