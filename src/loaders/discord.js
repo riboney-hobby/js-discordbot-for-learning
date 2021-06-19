9304841f889eb6778bf8fc2bf931bb9788eecc4e
riboney <14286113+Ribby-0254@users.noreply.github.com>
2021-06-19 02:45:36 -0400

    Changes discord.js to use Commando

--- src/loaders/discord.js
+++ src/loaders/discord.js
@@ -1,4 +1,14 @@
 const BotClient = require("../discord/BotClient.js");
+const logger = require("../logger.js");
+const path = require("path");
+
+// project root path
+const projectRoot = () => {
+  const currentPathArr = __dirname.split("/");
+  const indexOfSrc = currentPathArr.findIndex((item) => item === "src") + 1;
+  const projectRootArr = currentPathArr.slice(1, indexOfSrc);
+  return projectRootArr.join("/");
+};
 
 const config = {
   botToken: process.env.BOT_TOKEN,
@@ -13,4 +23,18 @@ const presenceData = {
   activity: activity,
 };
 
-module.exports = new BotClient(config, { presence: presenceData });
+const client = new BotClient(config);
+
+client.on("ready", () => {
+  logger.info("Logged in!");
+  client.user.setPresence(presenceData);
+});
+
+client.registry
+  .registerDefaultTypes()
+  .registerGroups([["bot-info", "Info on bot"]])
+  .registerDefaultGroups()
+  .registerDefaultCommands()
+  .registerCommandsIn(path.join("/", projectRoot(), "discord/commands/"));
+
+module.exports = client;
--- src/loaders/index.js
+++ src/loaders/index.js
@@ -1,7 +1,5 @@
 const bot = require("./discord.js");
 
 module.exports = async () => {
-  console.log("Starting up discord bot!");
   await bot.login();
-  console.log("Finished loading up bot!");
 };
